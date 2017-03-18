8ad445b813398498ad3e2f39102b287040665f9f
Meddeb Mohamed Fakher <meddebmohamedfakher@MacBook-Pro-de-Meddeb-Mohamed-Fakher.local>
2017-03-18 20:22:27 +0100

    Fix flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -41,7 +41,7 @@ def inject_static_media_url():
 import webapp.views
 
 
- if __name__ == '__main__':
-     app.debug = True
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
